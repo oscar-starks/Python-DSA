d0bc2f783539bfdcfb48b946cd36e2ff0759aefa
Blessed Oscar <oscarblessed04@gmail.com>
2022-10-16 23:59:30 +0100

    regex added

--- learning_regex.py
+++ learning_regex.py
@@ -34,5 +34,26 @@ phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number 415-555-4342 is 415-555-4242.')
 print("the first occurence of our search is ", mo.group())
 
-    
-    
+# lesson 6
+# this divides the search pattern into smaller groups and recieves indexes as arguments with the whole string
+# having the index of 0. The "groups()" method returns all the groups at once
+regex = re.compile(r"(\d\d\d)-(\d\d\d-\d\d\d\d)")
+mo = regex.search("my number is 123-456-7890")
+print(mo.group(2))
+print(mo.groups())
+
+# lesson 7
+# this matches a string with its possible different occurences using the pipe(|) operator
+# and I can check the part that was matched in the group() function
+regex = re.compile('Spider(man|woman|girl|chicken)')
+searching = regex.search("Spiderman is the king")
+print(searching.group(1))
+
+# this section basically checks conditionally. the part of the string inside the brackets is checked only if present
+rex = re.compile(r"batsoldier(s)?")
+searches = rex.search("that is the batsoldiers we talk about")
+print(searches.group())
+
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+regex = re.compile(r"bat(man)?woman")
+# searching = regex.search("that is the batman we talk about")
+# print(s
