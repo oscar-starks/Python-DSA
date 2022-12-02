37b106d05f6ae4e34a16846f7dfc3bee39cf5398
Blessed <oscarblessed04@gmail.com>
2022-12-02 11:50:57 +0100

    implemented daemon threads

--- learning_multithreading.py
+++ learning_multithreading.py
@@ -1,2 +1,36 @@
-from threading import Thread
+from threading import Thread, current_thread
 import time
+
+def calcsqr(num):
+    for i in num:
+        time.sleep(0.2)
+        print(current_thread().getName())
+        print(f"the square of {i} is {i**2}\n")
+        
+def calccube(num):
+    for i in num:
+        time.sleep(0.2)
+        print(current_thread().getName())
+        print(f"the cube of {i} is {i**3}\n")
+        
+def daemon():
+    while True:
+        time.sleep(0.2)
+        print("this is a daemon thread!")
+
+nums = [3,4,6,7]
+time1 = time.time()        
+f1 = Thread(target=calcsqr, name = "1st thread", args=(nums,))
+f2 = Thread(target=calccube, name = "2nd thread", args=(nums,))
+f3 = Thread(target=daemon, name = "3rd thread", daemon= True)
+
+f1.start()
+f2.start()
+f3.start()
+
+# f1.join()
+# f2.join()
+# f3.join()
+print("total time taken", time.time() - time1)
+
+# The command "Tasklist" can be used on our cmd to list out the number of cpu processes that are active on our computer
